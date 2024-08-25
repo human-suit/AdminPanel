9e42a1f325e06cfb020a3bc23bafa4480e48e8f7
Suit <time_and_work@mail.ru>
2024-08-26 02:59:01 +0300

    Validate

--- client/src/components/bloks_startap.jsx
+++ client/src/components/bloks_startap.jsx
@@ -35,32 +35,123 @@ const Bloks = ({ value }) =>{
     const [inx, setInx] = useState(1);
 
 
+    //валидация форм
+    const [srokiDirty2, setSrokiDirty2] = useState(false)
+    const [srokiError2, setSrokiError2] = useState("")
+    const [priceDirty2, setPriceDirty2] = useState(false)
+    const [priceError2, setPriceError2] = useState("")
+
+    const [formValid2, setFormValid2] = useState(false)
+
+    const [profe, setProfe] = useState(false)
+
+
+
     const handleChange = ({ target: {value, }}) =>{
         setValues({...values, [Name_title]: value})
+        if(values[Name_title] == "" || values[Name_title] == " "){
+            setProfe(false)
+        } else if (values[Name_title] != ""){
+            setProfe(true)
+        } else {
+            setProfe(false)
+        }
     }
     const handleChange2 = ({ target: {value, }}) =>{
         setValues({...values, [Description]: value})
+        if(values[Description] == "" || values[Description] == " "){
+            setProfe(false)
+        } else if (values[Description] != ""){
+            setProfe(true)
+        } else {
+            setProfe(false)
+        }
     }
     const handleChange3 = ({ target: {value, }}) =>{
         setValues({...values, [Name_treb]: value})
+        if(values[Name_treb] == "" || values[Name_treb] == " "){
+            setProfe(false)
+        } else if (values[Name_treb] != ""){
+            setProfe(true)
+        } else {
+            setProfe(false)
+        }
     }
     const handleChange4 = ({ target: {value, }}) =>{
         setValues({...values, [Name_komp]: value})
+        if(values[Name_komp] == "" || values[Name_komp] == " "){
+            setProfe(false)
+        } else if (values[Name_komp] != ""){
+            setProfe(true)
+        } else {
+            setProfe(false)
+        }
     }
     const handleChange5 = ({ target: {value, }}) =>{
         setValues({...values, [Sroki]: value})
+        let myLet = values[Sroki]
+        let myNum =Number(value)?true:false
+        if (myNum == false) {
+            alert('Неккоректное значения поля (числовое)')
+            setSrokiError2("Замечены некоректные данные")
+            setProfe(false)
+         } else{
+            if(myNum.length <= 0){
+                setSrokiError2("Замечены некоректные данные")
+                setProfe(false)
+            } else {
+                setSrokiError2("")
+                setProfe(true)
+            }
+         }
     }
     const handleChange6 = ({ target: {value, }}) =>{
         setValues({...values, [Price]: value})
+        let myLet = values[Price]
+        let myNum =Number(value)?true:false
+        if (myNum == false) {
+            alert('Неккоректное значения поля (числовое)')
+            setPriceError2("Замечены некоректные данные")
+            setProfe(false)
+         } else{
+            if(myNum.length <= 0){
+                setPriceError2("Замечены некоректные данные")
+                setProfe(false)
+            } else {
+                setPriceError2("")
+                setProfe(true)
+            }
+         }
     }
     const handleChange7 = ({ target: {value, }}) =>{
         setValues({...values, [Url_str]: value})
+        if(values[Url_str] == "" || values[Url_str] == " "){
+            setProfe(false)
+        } else if (values[Url_str] != ""){
+            setProfe(true)
+        } else {
+            setProfe(false)
+        }
     }
 
     var id = 0
    
     useEffect(()=> {
         
+
+        if(srokiError2 == "Замечены некоректные данные"){
+            setFormValid2(false)
+        } else{
+            setFormValid2(true)
+        }
+        if(priceError2 == "Замечены некоректные данные"){
+            setFormValid2(false)
+        } else{
+            setFormValid2(true)
+        }
+        
+
+
         // отправляем данные на сервер
         // socket.emit('join', ({state})=>{
             
@@ -84,7 +175,7 @@ const Bloks = ({ value }) =>{
         }, 4500);
 
         
-    }, [])
+    }, [srokiError2, priceError2])
 
     
     
@@ -97,144 +188,6 @@ const Bloks = ({ value }) =>{
     
     
 
-    const handleUp = (e) =>{
-        e.preventDefault()
-        
-        if(stage<date.length/3){
-            
-            const num = stage+1
-            
-            // setActive(!isActive);
-            setStage(num)
-            if(num>3 && num<(date.length/3)-2){
-                const int = inx+1
-                
-                var allElements = document.getElementsByClassName(`${styles.bu}`);
-                for(var i=0; i < allElements.length; i++)
-                {
-                    
-                    var item = allElements[i];
-                    var ints =  item.id 
-                    if(num==ints){
-                        console.log(num)
-                        const nax = num-1
-                        document.getElementById(nax).classList.add(`${styles.per}`);
-                        setInx(int)
-                    }
-                }
-                 var allElements = document.getElementsByClassName(`${styles.per}`);
-                for(var i=0; i < allElements.length; i++)
-                {
-                    var item = allElements[i];
-                    var ints =  item.id
-                    const nax = num-1 
-                    if(nax==ints){
-                        
-                    } else{
-                        document.getElementById(ints).classList.remove(`${styles.per}`);
-                    }
-                    
-                    
-                }
-            }
-            else{
-                var allElements = document.getElementsByClassName(`${styles.bu}`);
-                for(var i=0; i < allElements.length; i++)
-                {
-                    console.log(inx)
-                    var item = allElements[i];
-                    var ints =  item.id 
-                    if((num)==ints){
-                        document.getElementById(num).classList.add(`${styles.per}`);
-                    }
-                }
-                 var allElements = document.getElementsByClassName(`${styles.per}`);
-                for(var i=0; i < allElements.length; i++)
-                {
-                    var item = allElements[i];
-                    var ints =  item.id 
-                    console.log(num)
-                    if(num==ints){
-                        
-                    } else{
-                        document.getElementById(ints).classList.remove(`${styles.per}`);
-                    }
-                    
-                    
-                }
-            }
-            
-            
-           
-        }
-        
-        
-    }
-    const handleDown = (e) =>{
-        e.preventDefault()
-        if(stage>1){
-            const num = stage-1
-            document.getElementById(num).classList.add(`${styles.per}`);
-            setStage(num)
-            
-            if(num-2>0 && num <5){
-                const int = inx-1
-                
-                var allElements = document.getElementsByClassName(`${styles.bu}`);
-                for(var i=0; i < allElements.length; i++)
-                {
-                    
-                    var item = allElements[i];
-                    var ints =  item.id 
-                    if(num==ints){
-                        console.log(num)
-                        const nax = num-1
-                        document.getElementById(nax).classList.add(`${styles.per}`);
-                        setInx(int)
-                    }
-                }
-                 var allElements = document.getElementsByClassName(`${styles.per}`);
-                for(var i=0; i < allElements.length; i++)
-                {
-                    var item = allElements[i];
-                    var ints =  item.id
-                    const nax = num-1 
-                    if(nax==ints){
-                        
-                    } else{
-                        document.getElementById(ints).classList.remove(`${styles.per}`);
-                    }
-                    
-                    
-                }
-               
-            }
-            else{
-                var allElements = document.getElementsByClassName(`${styles.per}`);
-                for(var i=0; i < allElements.length; i++)
-                {
-                    var item = allElements[i];
-                    var int =  item.id 
-                    console.log(num) 
-                    if(num==int){
-                       
-                    } else{
-                        document.getElementById(int).classList.remove(`${styles.per}`);
-                    }
-                    
-                    
-                }
-            }
-            
-        }
-        
-        
-    }
-    
-
-    const up = (event) =>{
-        alert(myHandler.caller.arguments[0].target.id)
-    }
     const date = state.pro
     const dat = date
     const count = []
@@ -261,7 +214,16 @@ const Bloks = ({ value }) =>{
         location.reload();
     }
 
-    
+    const blurHandler2 = (e) =>{
+        switch(e.target.name){
+            case "sroki":
+                setSrokiDirty2(true)
+                break
+            case "price":
+                setPriceDirty2(true)
+                break
+        }
+    }
     
     
     
@@ -422,18 +384,23 @@ const Bloks = ({ value }) =>{
                                 <input  type="text" value={values[Description]} onChange={handleChange2} autoComplete="off" name="description" placeholder="Описание" required />
                                 <input  type="text" value={values[Name_treb]} onChange={handleChange3} autoComplete="off" name="name_treb" placeholder="Требования" required />
                                 <input  type="text" value={values[Name_komp]} onChange={handleChange4} autoComplete="off" name="name_komp" placeholder="Наименовании компании" required />
-                                <input  type="number" value={values[Sroki]} onChange={handleChange5} autoComplete="off" name="sroki" placeholder="Срок выполнения" required />
-                                <input  type="number" value={values[Price]} onChange={handleChange6} autoComplete="off" name="price" placeholder="Стоимость" required />
+                                {(srokiDirty2 && srokiError2) && <div style={{color:"red"}}>{srokiError2}</div>}
+                                <input onBlur={e => blurHandler2(e)}   type="number" value={values[Sroki]} onChange={handleChange5} autoComplete="off" name="sroki" placeholder="Срок выполнения" required />
+                                {(priceDirty2 && priceError2) && <div style={{color:"red"}}>{priceError2}</div>}
+                                <input onBlur={e => blurHandler2(e)}  type="number" value={values[Price]} onChange={handleChange6} autoComplete="off" name="price" placeholder="Стоимость" required />
                                 <input  type="text" value={values[Url_str]} onChange={handleChange7} autoComplete="off" name="url_str" placeholder="Ссылка" required />
                             </div>
                             <div className={styles.popup__but5}>
+                            { profe == true ? 
+                                
                                 <button 
                                     type="submit"
                                     onClick={redact}
-                                
+                                    disabled={!formValid2}
                                 >
                                     Отредактировать
                                 </button>
+                            : <div></div>}
                             </div>
                         
                     </div>
--- client/src/components/main_stage.jsx
+++ client/src/components/main_stage.jsx
@@ -30,38 +30,221 @@ const Main = () =>{
     const { search } = useLocation()
     const [values, setValues] = useState({[Name_title]: "",[Description]: "",[Name_treb]: "",[Name_komp]: "",[Sroki]: "",[Price]: "", [Del_id]: "", [Hide_id]: "", [Url_str]: "", [Open_id]: ""});
     const [val, setVal] = useState("")
-    
+
+    //валидация форм
+    const [nameDirty, setNameDirty] = useState(false)
+    const [nameError, setNameError] = useState("Не может быть пустым")
+    const [descriptionDirty, setDescriptionDirty] = useState(false)
+    const [descriptionError, setDescriptionError] = useState("Не может быть пустым")
+    const [trebDirty, setTrebDirty] = useState(false)
+    const [trebError, setTrebError] = useState("Не может быть пустым")
+    const [kompDirty, setKompDirty] = useState(false)
+    const [kompError, setKompError] = useState("Не может быть пустым")
+    const [srokiDirty, setSrokiDirty] = useState(false)
+    const [srokiError, setSrokiError] = useState("Не может быть пустым")
+    const [priceDirty, setPriceDirty] = useState(false)
+    const [priceError, setPriceError] = useState("Не может быть пустым")
+    const [delDirty, setDelDirty] = useState(false)
+    const [delError, setDelError] = useState("Не может быть пустым")
+    const [hideDirty, setHideDirty] = useState(false)
+    const [hideError, setHideError] = useState("Не может быть пустым")
+    const [urlDirty, setUrlDirty] = useState(false)
+    const [urlError, setUrlError] = useState("Не может быть пустым")
+    const [idDirty, setIdDirty] = useState(false)
+    const [idError, setIdError] = useState("Не может быть пустым")
+
+    const [formValid, setFormValid] = useState(false)
+    const [formValidHide, setFormValidHide] = useState(false)
+    const [formValidId, setFormValidId] = useState(false)
+    const [formValidDel, setFormValidDel] = useState(false)
+
+    useEffect(()=> {
+        if(nameError || descriptionError || trebError || kompError || srokiError || priceError || urlError){
+            setFormValid(false)
+        } else{
+            setFormValid(true)
+        }
+        if(hideError){
+            setFormValidHide(false)
+        } else{
+            setFormValidHide(true)
+        }
+        if(idError){
+            setFormValidId(false)
+        } else{
+            setFormValidId(true)
+        }
+        if(delError){
+            setDelError(false)
+        } else{
+            setDelError(true)
+        }
+        socket.on('error', (data) => {  
+            alert(data)
+         });
+
+
+    }, [nameError, descriptionError, trebError, kompError, srokiError, priceError, delError, hideError, urlError, idError])
+
+
+
     const [params, setParams] = useState("")
 
     const handleChange = ({ target: {value, }}) =>{
         setValues({...values, [Name_title]: value})
+        if(values[Name_title].length <= 1){
+            setNameError("Некоректные данные")
+        } else {
+            setNameError("")
+        }
     }
     const handleChange2 = ({ target: {value, }}) =>{
         setValues({...values, [Description]: value})
+        if(values[Description].length <= 8){
+            setDescriptionError("В описании больше 8 символов")
+        } else {
+            setDescriptionError("")
+        }
     }
     const handleChange3 = ({ target: {value, }}) =>{
         setValues({...values, [Name_treb]: value})
+        if(values[Name_treb].length <= 1){
+            setTrebError("Некоректные данные")
+        } else {
+            setTrebError("")
+        }
     }
     const handleChange4 = ({ target: {value, }}) =>{
         setValues({...values, [Name_komp]: value})
+        if(values[Name_komp].length <= 1){
+            setKompError("Некоректные данные")
+        } else {
+            setKompError("")
+        }
     }
     const handleChange5 = ({ target: {value, }}) =>{
         setValues({...values, [Sroki]: value})
+        let myLet = values[Sroki]
+        let myNum =Number(value)?true:false
+        if (myNum == false) {
+            alert('Неккоректное значения поля (числовое)')
+            setSrokiError("Некоректные данные")
+         } else{
+            if(values[Sroki].length <= 0){
+                setSrokiError("Некоректные данные")
+            } else {
+                setSrokiError("")
+            }
+         }
+        
     }
     const handleChange6 = ({ target: {value, }}) =>{
         setValues({...values, [Price]: value})
+        let myLet = values[Price]
+        let myNum =Number(value)?true:false
+        if (myNum==false) {
+            alert('Неккоректное значения поля (числовое)')
+            setPriceError("Некоректные данные")
+         } else{
+            if(values[Price].length <= 0){
+                setPriceError("Некоректные данные")
+            } else {
+                setPriceError("")
+            }
+         }
+        
     }
     const handleChange7 = ({ target: {value, }}) =>{
         setValues({...values, [Del_id]: value})
+        let myLet = values[Del_id]
+        let myNum =Number(value)?true:false
+        if (myNum==false) {
+            alert('Неккоректное значения поля (числовое)')
+            setDelError("Некоректные данные")
+         } else{
+            if(values[Del_id].length <= 0){
+                setDelError("Некоректные данные")
+            } else {
+                setDelError("")
+            }
+         }
+        
     }
     const handleChange8 = ({ target: {value, }}) =>{
         setValues({...values, [Hide_id]: value})
+        let myLet = values[Hide_id]
+        let myNum =Number(value)?true:false
+        if (myNum==false) {
+            alert('Неккоректное значения поля (числовое)')
+            setHideError("Некоректные данные")
+         } else{
+            if(values[Hide_id].length <= 0){
+                setHideError("Некоректные данные")
+            } else {
+                setHideError("")
+            }
+         }
+        
     }
     const handleChange9 = ({ target: {value, }}) =>{
         setValues({...values, [Open_id]: value})
+        let myLet = values[Open_id]
+        let myNum =Number(value)?true:false
+        if (myNum==false) {
+            alert('Неккоректное значения поля (числовое)')
+            setIdError("Некоректные данные")
+         } else{
+            if(values[Open_id].length <= 0){
+                setIdError("Некоректные данные")
+            } else {
+                setIdError("")
+            }
+         }
+        
     }
     const handleChange10 = ({ target: {value, }}) =>{
         setValues({...values, [Url_str]: value})
+        if(values[Url_str].length <= 1){
+            setUrlError("Некоректные данные")
+        } else {
+            setUrlError("")
+        }
+    }
+
+    const blurHandler = (e) =>{
+        switch(e.target.name){
+            case "name_title":
+                setNameDirty(true)
+                break
+            case "description":
+                setDescriptionDirty(true)
+                break
+            case "name_treb":
+                setTrebDirty(true)
+                break
+            case "name_komp":
+                setKompDirty(true)
+                break
+            case "sroki":
+                setSrokiDirty(true)
+                break
+            case "price":
+                setPriceDirty(true)
+                break
+            case "del_id":
+                setDelDirty(true)
+                break
+            case "hide_id":
+                setHideDirty(true)
+                break
+            case "url_str":
+                setUrlDirty(true)
+                break
+            case "open_id":
+                setIdDirty(true)
+                break
+            
+        }
     }
 
 
@@ -82,14 +265,8 @@ const Main = () =>{
         location.reload();
     }
     
-    useEffect(()=> {
-
-        socket.on('error', (data) => {  
-            alert(data)
-         });
-
-
-    }, [])
+    
+    
     
     
     
@@ -160,19 +337,26 @@ const Main = () =>{
                         </div>
                         
                             <div className={styles.popup__text}>
-                                <input  type="text" value={values[Name_title]} onChange={handleChange} autoComplete="off" name="name_title" placeholder="Наименование" required />
-                                <input  type="text" value={values[Description]} onChange={handleChange2} autoComplete="off" name="description" placeholder="Описание" required />
-                                <input  type="text" value={values[Name_treb]} onChange={handleChange3} autoComplete="off" name="name_treb" placeholder="Требования" required />
-                                <input  type="text" value={values[Name_komp]} onChange={handleChange4} autoComplete="off" name="name_komp" placeholder="Наименовании компании" required />
-                                <input  type="number" value={values[Sroki]} onChange={handleChange5} autoComplete="off" name="sroki" placeholder="Срок выполнения" required />
-                                <input  type="number" value={values[Price]} onChange={handleChange6} autoComplete="off" name="price" placeholder="Стоимость" required />
-                                <input  type="text" value={values[Url_str]} onChange={handleChange10} autoComplete="off" name="url_str" placeholder="Ссылка" required />
+                                {(nameDirty && nameError) && <div style={{color:"red"}}>{nameError}</div>}
+                                <input onBlur={e => blurHandler(e)}  type="text" value={values[Name_title]} onChange={handleChange} autoComplete="off" name="name_title" placeholder="Наименование" required />
+                                {(descriptionDirty && descriptionError) && <div style={{color:"red"}}>{descriptionError}</div>}
+                                <input onBlur={e => blurHandler(e)}  type="text" value={values[Description]} onChange={handleChange2} autoComplete="off" name="description" placeholder="Описание" required />
+                                {(trebDirty && trebError) && <div style={{color:"red"}}>{trebError}</div>}
+                                <input onBlur={e => blurHandler(e)}  type="text" value={values[Name_treb]} onChange={handleChange3} autoComplete="off" name="name_treb" placeholder="Требования" required />
+                                {(kompDirty && kompError) && <div style={{color:"red"}}>{kompError}</div>}
+                                <input onBlur={e => blurHandler(e)}  type="text" value={values[Name_komp]} onChange={handleChange4} autoComplete="off" name="name_komp" placeholder="Наименовании компании" required />
+                                {(srokiDirty && srokiError) && <div style={{color:"red"}}>{srokiError}</div>}
+                                <input onBlur={e => blurHandler(e)}  type="number" value={values[Sroki]} onChange={handleChange5} autoComplete="off" name="sroki" placeholder="Срок выполнения" required />
+                                {(priceDirty && priceError) && <div style={{color:"red"}}>{priceError}</div>}
+                                <input onBlur={e => blurHandler(e)}  type="number" value={values[Price]} onChange={handleChange6} autoComplete="off" name="price" placeholder="Стоимость" required />
+                                {(urlDirty && urlError) && <div style={{color:"red"}}>{urlError}</div>}
+                                <input onBlur={e => blurHandler(e)}  type="text" value={values[Url_str]} onChange={handleChange10} autoComplete="off" name="url_str" placeholder="Ссылка" required />
                             </div>
                             <div className={styles.popup__but}>
                                 <button 
                                     type="submit"
                                     onClick={addStart}
-                                
+                                    disabled={!formValid}
                                 >
                                     Добавить
                                 </button>
@@ -190,13 +374,14 @@ const Main = () =>{
                             Удалить 
                         </div>
                         <div className={styles.popup__text2}>
-                            <input  type="number" value={values[Del_id]} onChange={handleChange7} autoComplete="off" name="del_id" placeholder="Айди" required />
+                            {(delDirty && delError) && <div style={{color:"red"}}>{delError}</div>}
+                            <input onBlur={e => blurHandler(e)}   type="number" value={values[Del_id]} onChange={handleChange7} autoComplete="off" name="del_id" placeholder="Айди" required />
                         </div>
                         <div className={styles.popup__but}>
                             <button 
                                 type="submit"
                                 onClick={deletStart}
-                            
+                                disabled={!formValidDel}
                             >
                                 Удалить
                             </button>
@@ -213,13 +398,14 @@ const Main = () =>{
                             Скрыть запись 
                         </div>
                         <div className={styles.popup__text3}>
-                            <input  type="number" value={values[Hide_id]} onChange={handleChange8} autoComplete="off" name="del_id" placeholder="Айди" required />
+                            {(hideDirty && hideError) && <div style={{color:"red"}}>{hideError}</div>}
+                            <input onBlur={e => blurHandler(e)}   type="number" value={values[Hide_id]} onChange={handleChange8} autoComplete="off" name="del_id" placeholder="Айди" required />
                         </div>
                         <div className={styles.popup__but}>
                             <button 
                                 type="submit"
                                 onClick={hide}
-                            
+                                disabled={!formValidHide}
                             >
                                 Скрыть
                             </button>
@@ -236,13 +422,14 @@ const Main = () =>{
                             Открыть запись
                         </div>
                         <div className={styles.popup__text4}>
-                            <input  type="number" value={values[Open_id]} onChange={handleChange9} autoComplete="off" name="del_id" placeholder="Айди" required />
+                            {(idDirty && idError) && <div style={{color:"red"}}>{idError}</div>}
+                            <input onBlur={e => blurHandler(e)}   type="number" value={values[Open_id]} onChange={handleChange9} autoComplete="off" name="del_id" placeholder="Айди" required />
                         </div>
                         <div className={styles.popup__but}>
                             <button 
                                 type="submit"
                                 onClick={open}
-                            
+                                disabled={!formValidId}
                             >
                                 Открыть
                             </button>
